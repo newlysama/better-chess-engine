206c47abc114d8572de050c5a7a9936345f501bb
newlysama <thibault.thomas71@gmail.com>
2025-06-14 22:45:46 +0200

    fix: comment typo

--- src/conf/types.h
+++ src/conf/types.h
@@ -22,7 +22,7 @@ namespace conf::types
 {
     /**
      * @typedef pieceBoards
-     * @brief 2x6 array of pieces bitboards (1 for each piece from each piece)
+     * @brief 2x6 array of pieces bitboards (1 for each piece from each team)
      */
     typedef std::array<std::array<engine::Bitboard, conf::enums::Piece::PIECES>, conf::enums::Team::COLORS> pieceBitboards;
 } // namespace conf::types
