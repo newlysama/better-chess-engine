294a50c4afdcd2088ac1fafb9bb79c6c62475c34
newlysama <thibault.thomas71@gmail.com>
2025-06-14 14:28:56 +0200

    add: docs

--- src/logging/include/logging.h
+++ src/logging/include/logging.h
@@ -1,3 +1,10 @@
+/**
+ * @file logging.h
+ * @author Thibault THOMAS
+ *
+ * @brief Logging system management
+ */
+
 #ifndef LOGGING_H_
 #define LOGGING_H_
 
@@ -7,21 +14,37 @@
 /*----- External Libs -----*/
 #include <spdlog/spdlog.h>
 
+/**
+ * @brief Declares loggers initialization and access
+ */
 namespace logging
 {
 
-    // Initialize loggers
+    /**
+     * @brief Initialize engine's logger
+     */
     void init_engine_logger();
+
+    /**
+     * @brief Initialize app's logger
+     */
     void init_application_logger();
 
-    // Access loggers
+    /**
+     * @brief Allows engine's logger access
+     */
     std::shared_ptr<spdlog::logger> engine_logger();
+
+    /**
+     * @brief Allows app's logger access
+     */
     std::shared_ptr<spdlog::logger> application_logger();
 
 } // namespace logging
 
-// Utility maccros for logging
-
+/**
+ * @brief Maccros to ensure simple access to loggers through the program
+ */
 #define LOG_ENGINE_DEBUG(...)    SPDLOG_LOGGER_DEBUG    (logging::engine_logger(), __VA_ARGS__)
 #define LOG_ENGINE_INFO(...)     SPDLOG_LOGGER_INFO     (logging::engine_logger(), __VA_ARGS__)
 #define LOG_ENGINE_WARN(...)     SPDLOG_LOGGER_WARN     (logging::engine_logger(), __VA_ARGS__)
