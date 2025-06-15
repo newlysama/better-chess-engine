21cc2a97c2463379674bdee3dff65cda6d3b94db
newlysama <thibault.thomas71@gmail.com>
2025-06-15 10:27:01 +0200

    fix: getSquareIndex()'s doc / add: getRankIndex() - getFileIndex() + bitboards attributes

--- src/engine/include/board.h
+++ src/engine/include/board.h
@@ -14,7 +14,6 @@
 
 /*----- Project Headers -----*/
 #include "conf/types.h"
-#include "engine/include/bitboard.h"
 
 /**
  * @namespace engine
@@ -23,7 +22,7 @@ namespace engine
 {
     /**
      * @class Board
-     * @brief Board represented by multiple bitboards
+     * @brief Board represented by multiple Bitboards
      */
     class Board
     {
@@ -39,17 +38,45 @@ namespace engine
              */
             ~Board() = default;
 
+
+
             /**
-             * @brief Returns the index of a square, based on the rank's and file's indexes
+             * @brief Returns the a square's index, based on the rank's and file's indexes
              *
              * @param [in] rankInex  : rank's index
              * @param [in] fileIndex : file's index
+             * @return uint8_t : desired index
              */
-            static inline constexpr uint8_t getSquareIndex(uint8_t & rankIndex, uint8_t & fileIndex) noexcept;
+            static inline constexpr uint8_t getSquareIndex(uint8_t& rankIndex, uint8_t& fileIndex) noexcept { return (8 * rankIndex) + fileIndex; };
+
+            /**
+             * @brief Returns a rank's index, base on a square's index
+             *
+             * @param [in] squareIndex : square's index
+             * @return uint8_t : disired index
+             */
+            static inline constexpr uint8_t getRankIndex(uint8_t& squareIndex) noexcept { return squareIndex >> 3; };
+
+            /**
+             * @brief Returns a files's index, base on a square's index
+             *
+             * @param [in] squareIndex : square's index
+             * @return uint8_t : disired index
+             */
+            static inline constexpr uint8_t getFileIndex(uint8_t& squareIndex) noexcept { return squareIndex & 7; };
 
         private:
-            // Bitboards for each piece of each team
-            std::unique_ptr<conf::types::pieceBitboards> pieceBB;
+            // Bitboards for each piece from each team
+            conf::types::piecesBitboards piecesBB;
+
+            // Bitboards for each piece from 1 team
+            conf::types::teamPiecesBitboards whitePiecesBB;
+            conf::types::teamPiecesBitboards blackPiecesBB;
+
+            // Occupancy of the board (1 general + 1 for each team)
+            conf::types::occupancy generalOccupancy;
+            conf::types::occupancy whiteOccupancy;
+            conf::types::occupancy blackOccupancy;
     };
 } // namespace engine
 
