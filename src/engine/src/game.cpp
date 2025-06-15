b1fe5854b2e45a795aff9c28790210ec48a31728
newlysama <thibault.thomas71@gmail.com>
2025-06-15 10:28:53 +0200

    add: Game class implem squeletton

---
+++ src/engine/src/game.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file game.cpp
+ * @author Thibault THOMAS
+ *
+ * @brief Game class implementation
+ */
+
+/*----- Project Headers -----*/
+#include "engine/include/game.h"
+
+/**
+ * @namespace engine
+ */
+namespace engine
+{
+
+} // namespace engine
