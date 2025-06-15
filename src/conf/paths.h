3d6f023875971ca7d91714ddb194e8f21ce1ec1e
newlysama <thibault.thomas71@gmail.com>
2025-06-15 16:47:40 +0200

    delete: constexpr for file paths

--- src/conf/paths.h
+++ src/conf/paths.h
@@ -21,11 +21,11 @@ namespace conf::paths
     #ifdef PROJECT_ROOT
 
     // Path to the directory from where the binary is executed
-    inline constexpr std::filesystem::path root = std::filesystem::path(PROJECT_ROOT);
+    inline const std::filesystem::path root = std::filesystem::path(PROJECT_ROOT);
 
     // Paths to log files
-    inline constexpr std::string engine_log_file = (root / "logs" / "engine.log").string();
-    inline constexpr std::string app_log_file    = (root / "logs" / "app.log").string();
+    inline const std::string engine_log_file = (root / "logs" / "engine.log").string();
+    inline const std::string app_log_file    = (root / "logs" / "app.log").string();
 
     #endif // PROJECT_ROOT
 } // namespace conf::paths
