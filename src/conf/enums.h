e6b0e66ea3e16ce64ee49b6ab360d3d9f070b11c
newlysama <thibault.thomas71@gmail.com>
2025-06-15 10:23:03 +0200

    add: Files and Ranks enums

--- src/conf/enums.h
+++ src/conf/enums.h
@@ -38,6 +38,40 @@ namespace conf::enums
         BLACK  = 1,
         COLORS = 2
     };
+
+    /**
+     * @enum Ranks
+     * @brief Enum of board's ranks
+     */
+    enum Ranks
+    {
+        RANK_1 = 0,
+        RANK_2 = 1,
+        RANK_3 = 2,
+        RANK_4 = 3,
+        RANK_5 = 4,
+        RANK_6 = 5,
+        RANK_7 = 6,
+        RANK_8 = 7,
+        RANKS  = 8,
+    };
+
+    /**
+     * @enum Files
+     * @brief Enum of board's files
+     */
+    enum Files
+    {
+        FILE_A = 0,
+        FILE_B = 1,
+        FILE_C = 2,
+        FILE_D = 3,
+        FILE_E = 4,
+        FILE_F = 5,
+        FILE_G = 6,
+        FILE_H = 7,
+        FILES  = 8,
+    };
 } // namespace conf::enums
 
 #endif // ENUMS_H_
