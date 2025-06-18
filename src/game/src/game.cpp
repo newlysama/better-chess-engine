63afa4f6823398e14c213290f6211e78c92a6aff
newlysama <thibault.thomas71@gmail.com>
2025-06-18 19:49:02 +0200

    add: constructor's implem

--- src/game/src/game.cpp
+++ src/game/src/game.cpp
@@ -18,6 +18,20 @@ namespace engine
      */
     namespace game
     {
+        Game::Game() noexcept
+            : turnCount(0)
+            , colorToPlay(conf::enums::Colors::WHITE)
+            , board(board::Board())
+            , legalMoves{}
+            {
+                using namespace conf::enums;
 
+                // Reserve predertermined space for our vectors of legal moves
+                legalMoves[MoveTypes::QUIET]     .reserve(RESERVE_QUIET);
+                legalMoves[MoveTypes::CAPTURE]   .reserve(RESERVE_CAPTURES);
+                legalMoves[MoveTypes::PROMOTION] .reserve(RESERVE_PROMOTIONS);
+                legalMoves[MoveTypes::CASTLE]    .reserve(RESERVE_CASTLES);
+                legalMoves[MoveTypes::ENPASSANT] .reserve(RESERVE_ENPASSANT);
+            }
     } // namespace game
 } // namespace engine
