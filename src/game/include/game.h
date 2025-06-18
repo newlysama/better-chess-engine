13f02b9ed9b5bf6dee5eb8c8a5f98175c2ae4c3e
newlysama <thibault.thomas71@gmail.com>
2025-06-18 19:48:13 +0200

    add: constructors, board and legalMoves members, sizes of legal moves vectors

--- src/game/include/game.h
+++ src/game/include/game.h
@@ -13,7 +13,9 @@
 
 /*----- Project Headers -----*/
 #include "conf/enums.h"
+#include "conf/types.h"
 #include "board/include/board.h"
+#include "move/include/move_generator.h"
 
 /**
  * @namespace engine
@@ -32,15 +34,38 @@ namespace engine
         class Game
         {
             public:
-                constexpr Game() noexcept;
+
+                /**
+                 * @brief Constructor.
+                 */
+                Game() noexcept;
+
+                /**
+                 * @brief Constructor from FEN notation.
+                 */
+                Game(const std::string fenNotation) noexcept;
+
+                /**
+                 * @brief Default destructor.
+                 */
                 ~Game() noexcept = default;
 
-            private:
-                uint16_t turnCount;               //< Number of played tunred
-                conf::enums::Colors colorsToPlay; //< Whose turn is it ? :)
 
-                engine::board::Board board; //< Game's board
+            private:
+                uint16_t turnCount;              // Number of played tunred
+                conf::enums::Colors colorToPlay; // Whose turn is it ? :)
+
+                engine::board::Board board; // Game's board
+
+                conf::types::LegalMoves legalMoves; // Vectors of legal moves
         };
+
+        // Constant variables defining the sizes to reserve for our vectors of legal moves
+        constexpr size_t RESERVE_QUIET      = 64;
+        constexpr size_t RESERVE_CAPTURES   = 32;
+        constexpr size_t RESERVE_PROMOTIONS = 32;
+        constexpr size_t RESERVE_CASTLES    = 2;
+        constexpr size_t RESERVE_ENPASSANT  = 1;
     } // namespace game
 } // namespace engine
 
