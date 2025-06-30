0f7dd319389b7b2b4c5fcfa74b32920babf60c82
newlysama <thibault.thomas71@gmail.com>
2025-06-30 21:21:00 +0200

    fix: merged namespaces

--- src/engine/board/board.cpp
+++ src/engine/board/board.cpp
@@ -10,45 +10,39 @@
 #include "engine/board/board.h"
 
 /**
- * @namespace engine
+ * @namespace engine::board
  */
-namespace engine
+namespace engine::board
 {
-    /**
-     * @namespace board
-     */
-    namespace board
+    Board::Board() noexcept
+        : allPieces{{// -- White pieces (index 0) --
+                     {
+                         Bitboard{0x0000'0000'0000'FF00ULL}, // pawns
+                         Bitboard{0x0000'0000'0000'0042ULL}, // knights
+                         Bitboard{0x0000'0000'0000'0024ULL}, // bishops
+                         Bitboard{0x0000'0000'0000'0081ULL}, // rooks
+                         Bitboard{0x0000'0000'0000'0008ULL}, // queen
+                         Bitboard{0x0000'0000'0000'0010ULL}  // king
+                     },
+                     // -- Black pieces (index 1) --
+                     {
+                         Bitboard{0x00FF'0000'0000'0000ULL}, // pawns
+                         Bitboard{0x4200'0000'0000'0000ULL}, // knights
+                         Bitboard{0x2400'0000'0000'0000ULL}, // bishops
+                         Bitboard{0x8100'0000'0000'0000ULL}, // rooks
+                         Bitboard{0x0800'0000'0000'0000ULL}, // queen
+                         Bitboard{0x1000'0000'0000'0000ULL}  // king
+                     }}},
+          generalOccupancy{}, whiteOccupancy{}, blackOccupancy{}
     {
-        Board::Board() noexcept
-            : allPieces{{// -- White pieces (index 0) --
-                         {
-                             Bitboard{0x0000'0000'0000'FF00ULL}, // pawns
-                             Bitboard{0x0000'0000'0000'0042ULL}, // knights
-                             Bitboard{0x0000'0000'0000'0024ULL}, // bishops
-                             Bitboard{0x0000'0000'0000'0081ULL}, // rooks
-                             Bitboard{0x0000'0000'0000'0008ULL}, // queen
-                             Bitboard{0x0000'0000'0000'0010ULL}  // king
-                         },
-                         // -- Black pieces (index 1) --
-                         {
-                             Bitboard{0x00FF'0000'0000'0000ULL}, // pawns
-                             Bitboard{0x4200'0000'0000'0000ULL}, // knights
-                             Bitboard{0x2400'0000'0000'0000ULL}, // bishops
-                             Bitboard{0x8100'0000'0000'0000ULL}, // rooks
-                             Bitboard{0x0800'0000'0000'0000ULL}, // queen
-                             Bitboard{0x1000'0000'0000'0000ULL}  // king
-                         }}},
-              generalOccupancy{}, whiteOccupancy{}, blackOccupancy{}
+        // construit les bitboards d'occupancy
+        for (size_t piece = 0; piece < conf::enums::Pieces::PIECES; piece++)
         {
-            // construit les bitboards d'occupancy
-            for (size_t piece = 0; piece < conf::enums::Pieces::PIECES; piece++)
-            {
-                whiteOccupancy |= allPieces[conf::enums::Colors::WHITE][piece];
-                blackOccupancy |= allPieces[conf::enums::Colors::BLACK][piece];
-            }
-
-            generalOccupancy = whiteOccupancy | blackOccupancy;
+            whiteOccupancy |= allPieces[conf::enums::Colors::WHITE][piece];
+            blackOccupancy |= allPieces[conf::enums::Colors::BLACK][piece];
         }
 
-    } // namespace board
-} // namespace engine
+        generalOccupancy = whiteOccupancy | blackOccupancy;
+    }
+
+} // namespace engine::board
--- src/engine/board/board.h
+++ src/engine/board/board.h
@@ -17,81 +17,90 @@
 #include "engine/board/bitboard.h"
 
 /**
- * @namespace engine
+ * @namespace engine::board
  */
-namespace engine
+namespace engine::board
 {
     /**
-     * @namespace board
+     * @class Board
+     * @brief Board represented by multiple Bitboards.
      */
-    namespace board
+    class Board
     {
+      public:
         /**
-         * @class Board
-         * @brief Board represented by multiple Bitboards.
+         * @brief Constructor.
          */
-        class Board
+        Board() noexcept;
+
+        /**
+         * @brief Constructor from FEN notation.
+         */
+        Board(const std::string fenNotation) noexcept;
+
+        /**
+         * @brief Destructor.
+         */
+        ~Board() = default;
+
+        /**
+         * @brief Get a square's index, based on the rank's and file's indexes.
+         *
+         * @param [in] rankInex  : rank's index
+         * @param [in] fileIndex : file's index
+         * @return uint8_t : the square's index
+         */
+        static inline constexpr uint8_t getSquareIndex(const uint8_t rankIndex, const uint8_t fileIndex) noexcept
         {
-          public:
-            /**
-             * @brief Constructor.
-             */
-            Board() noexcept;
-
-            /**
-             * @brief Constructor from FEN notation.
-             */
-            Board(const std::string fenNotation) noexcept;
-
-            /**
-             * @brief Destructor.
-             */
-            ~Board() = default;
-
-            /**
-             * @brief Get a square's index, based on the rank's and file's indexes.
-             *
-             * @param [in] rankInex  : rank's index
-             * @param [in] fileIndex : file's index
-             * @return uint8_t : the square's index
-             */
-            static inline constexpr uint8_t getSquareIndex(const uint8_t rankIndex, const uint8_t fileIndex) noexcept
-            {
-                return (8 * rankIndex) + fileIndex;
-            };
-
-            /**
-             * @brief Get a rank's index, bases on a square's index
-             *
-             * @param [in] squareIndex : square's index
-             * @return uint8_t : the rank's index
-             */
-            static inline constexpr uint8_t getRankIndex(const uint8_t squareIndex) noexcept
-            {
-                return squareIndex >> 3;
-            };
-
-            /**
-             * @brief Get a files's index, bases on a square's index.
-             *
-             * @param [in] squareIndex : square's index
-             * @return uint8_t : the file's index
-             */
-            static inline constexpr uint8_t getFileIndex(const uint8_t squareIndex) noexcept
-            {
-                return squareIndex & 7;
-            };
-
-            // Bitboards for each piece from each team
-            conf::types::PiecesBitboards allPieces;
-
-            // Occupancy of the board (1 general + 1 for each team)
-            board::Bitboard generalOccupancy;
-            board::Bitboard whiteOccupancy;
-            board::Bitboard blackOccupancy;
+            return (8 * rankIndex) + fileIndex;
         };
 
-    } // namespace board
-} // namespace engine
+        /**
+         * @brief Get a rank's index, bases on a square's index
+         *
+         * @param [in] squareIndex : square's index
+         * @return uint8_t : the rank's index
+         */
+        static inline constexpr uint8_t getRankIndex(const uint8_t squareIndex) noexcept
+        {
+            return squareIndex >> 3;
+        };
+
+        /**
+         * @brief Get a files's index, bases on a square's index.
+         *
+         * @param [in] squareIndex : square's index
+         * @return uint8_t : the file's index
+         */
+        static inline constexpr uint8_t getFileIndex(const uint8_t squareIndex) noexcept
+        {
+            return squareIndex & 7;
+        };
+
+        /**
+         * @brief Computes rook's sliding table.
+         */
+        inline void updateRookSlidingTable() noexcept;
+
+        /**
+         * @brief Computes bishop's sliding table.
+         */
+        inline void updateBishopSlidingTable() noexcept;
+
+        /**
+         * @brief Computes queen's sliding table.
+         */
+        inline void updateQueenlidingTable() noexcept;
+
+        // Bitboards for each piece from each team
+        conf::types::PiecesBitboards allPieces;
+
+        // Occupancy of the board (1 general + 1 for each team)
+        board::Bitboard generalOccupancy;
+        board::Bitboard whiteOccupancy;
+        board::Bitboard blackOccupancy;
+    };
+
+} // namespace engine::board
 
 #endif // BOARD_H_
