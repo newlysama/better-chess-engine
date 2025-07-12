5d7d035205caf3f5087124e0b32beccab8554430
newlysama <thibault.thomas71@gmail.com>
2025-07-12 11:28:10 +0200

    delete: legal mvoe vector / add: moveList attribute

--- src/engine/game/game.h
+++ src/engine/game/game.h
@@ -15,7 +15,7 @@
 #include "conf/enums.h"
 #include "conf/types.h"
 #include "engine/board/board.h"
-#include "engine/game/move_generator.h"
+#include "engine/game/move_list.h"
 
 /**
  * @namespace engine::game
@@ -44,26 +44,13 @@ namespace engine::game
          */
         ~Game() noexcept = default;
 
-        /**
-         * @brief Clears the vectors of legal moves.
-         */
-        inline void clearLegalMoves() noexcept;
-
       private:
         uint16_t turnCount;              // Number of played tunred
         conf::enums::Colors colorToPlay; // Whose turn is it ? :)
 
-        engine::board::Board board; // Game's board
-
-        conf::types::LegalMoves legalMoves; // Vectors of legal moves
+        engine::board::Board board;      // Game's board
+        engine::move::MoveList moveList; // List of moves
     };
-
-    // Constant variables defining the sizes to reserve for our vectors of legal moves
-    constexpr size_t RESERVE_QUIET = 64;
-    constexpr size_t RESERVE_CAPTURES = 32;
-    constexpr size_t RESERVE_PROMOTIONS = 32;
-    constexpr size_t RESERVE_CASTLES = 2;
-    constexpr size_t RESERVE_ENPASSANT = 1;
 
 } // namespace engine::game
 
