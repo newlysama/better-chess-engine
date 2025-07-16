63cf0e3e043e5db935ac28ea0a7defe81ccec387
newlysama <thibault.thomas71@gmail.com>
2025-07-16 20:39:51 +0200

    add: square bitboard index to string square conversion

---
+++ src/utils/utils.h
@@ -0,0 +1,45 @@
+/**
+ * @file      utils.h
+ * @author    Thibault THOMAS
+ * @copyright Copyright 2025 Better Chess Engine
+ * @par       This project is released under the MIT License
+ *
+ * @brief Utility functions.
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <algorithm>
+#include <string_view>
+
+#include "engine/core/const.h"
+#include "logging/logging.h"
+
+/**
+ * @namespace utils
+ */
+namespace utils
+{
+    /**
+     * @brief Retreives the rank/file string notation given a square index.
+     *
+     * @param [in] squareIndex : the index of the square within a Bitboard
+     * @return std::string_view : rank/file string notation or "Unkown" if squareIndex is not valid
+     */
+    inline constexpr std::string_view squareIndexToString(int squareIndex)
+    {
+        auto iter = std::find_if(std::begin(engine::core::SQUARE_INDEX), std::end(engine::core::SQUARE_INDEX),
+                                 [&squareIndex](auto&& pair) { return pair.second == squareIndex; });
+
+        if (iter == std::end(engine::core::SQUARE_INDEX))
+        {
+            LOG_ERROR("[utils::squareIndexToString()] Invalid square index: {}", squareIndex);
+            return "Unknown";
+        }
+
+        return iter->first;
+    }
+} // namespace utils
+
+#endif // UTILS_H_
