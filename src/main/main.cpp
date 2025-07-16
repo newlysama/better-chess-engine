2ad881ec7ee3bbb77a1f1fcbdfb67cedfc6b1e6a
newlysama <thibault.thomas71@gmail.com>
2025-07-16 20:38:41 +0200

    add: console game logic

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -17,6 +17,9 @@
 
 int main(int argc, char* argv[])
 {
+    (void)argc;
+    (void)argv;
+
     // Initialize loggers
     logging::init_logger();
 
@@ -25,8 +28,26 @@ int main(int argc, char* argv[])
 #else
 
     engine::game::Game game;
+    game.moveList.generateAllMoves(game.state, engine::core::Colors::WHITE);
+    engine::printer::printState(game.state);
 
-    engine::printer::printBoard(game.state);
+    while (true)
+    {
+        std::string userInput = game.askInput();
+
+        engine::game::Move move;
+        move = game.inputToMove(userInput);
+
+        if (move.isSet() == false)
+        {
+            std::cout << "Please enter a valid input";
+
+            continue;
+        }
+
+        game.playMove(move);
+        engine::printer::printState(game.state);
+    }
 
 #endif // BUILD_GENERATE_MAGICS
 
