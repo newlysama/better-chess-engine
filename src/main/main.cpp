06ff3805f4021ec382d45cfbe4db4069b7f54f95
newlysama <thibault.thomas71@gmail.com>
2025-07-18 18:52:24 +0200

    delete: playConsole logic

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,92 +8,29 @@
  */
 
 #include "engine/game/game.h"
-#include "engine/printer/printer.h"
 #include "logging/logging.h"
 
 #if defined(GENERATE_MAGICS)
 #include "engine/board/magics_generator.h"
 #endif
 
-#if defined(PLAY_CONSOLE)
-void playConsole()
-{
-    engine::game::Game game;
-    game.moveList.generateAllMoves(game.state);
-    engine::printer::printState(game.state);
-
-    while (true)
-    {
-        std::string userInput = game.askInput();
-        bool draw = false;
-
-        if (userInput.compare("draw") == 0)
-        {
-            LOG_DEBUG("Player {} is calling a draw", utils::toString(game.state.sideToMove));
-            if (game.state.halfMoveClock >= 100)
-            {
-                std::string drawResponse = "";
-                while (true)
-                {
-                    drawResponse = game.askDraw();
-
-                    if (drawResponse != "yes" && drawResponse != "no")
-                    {
-                        std::cout << "Please enter a valid response to the draw\n";
-                        continue;
-                    }
-                    else
-                    {
-                        if (drawResponse == "yes")
-                        {
-                            LOG_DEBUG("Bot players acctepted the draw. Ending the game.");
-                            std::cout << "Game ended on a draw.\n";
-                            return;
-                        }
-                        else
-                        {
-                            LOG_DEBUG("Enemy player refused the draw request, continuing.");
-                            std::cout << "Enemy player refused the draw request, continuing.\n";
-                            draw = true;
-                            break;
-                        }
-                    }
-                }
-            }
-        }
-
-        if (!draw)
-        {
-            engine::game::Move move;
-            move = game.inputToMove(userInput);
-
-            if (move.isSet() == false) [[unlikely]]
-            {
-                std::cout << "Please enter a valid input\n";
-
-                continue;
-            }
-
-            game.playMove(move);
-            engine::printer::printState(game.state);
-        }
-    }
-}
-#endif // PLAY_CONSOLE
-
 int main(int argc, char* argv[])
 {
     (void)argc;
     (void)argv;
 
-    // Initialize loggers
+    // Initialize logger
     logging::init_logger();
+
+    // Init game
+    engine::game::Game game;
 
 #if defined(GENERATE_MAGICS)
     engine::board::initMagics();
 #elif defined(PLAY_CONSOLE)
-    playConsole();
+    game.playConsole();
 #else
+    LOG_INFO("DEFAULT BUILD");
     return 0;
 #endif
 
