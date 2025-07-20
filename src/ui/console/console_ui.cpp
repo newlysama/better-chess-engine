418cb3ba8714098a77991d2948cc0a578d017c4d
newlysama <thibault.thomas71@gmail.com>
2025-07-20 10:22:56 +0200

    add: new console user interace

---
+++ src/ui/console/console_ui.cpp
@@ -0,0 +1,157 @@
+/**
+ * @file      console_ui.cpp
+ * @author    Thibault THOMAS
+ * @copyright Copyright 2025 Better Chess Engine
+ * @par       This project is released under the MIT License
+ *
+ * @brief Console rendering implem.
+ */
+
+#include "ui/console/console_ui.h"
+
+#include <fstream>
+#include <print>
+
+#include "conf/paths.h"
+#include "logging/logging.h"
+#include "utils/enums_to_string.h"
+
+/**
+ * @namespace ui::console
+ */
+namespace ui::console
+{
+    using namespace conf::paths;
+    using namespace engine::core;
+    using namespace engine::board;
+
+    void ConsoleUI::loadHeader() noexcept
+    {
+        // Load header file
+        std::ifstream headerFile(consoleUIHeaderPath.data());
+
+        if (!headerFile)
+        {
+            ConsoleUI::HEADER = "";
+            LOG_WARN("Could not open header file at: {}", consoleUIHeaderPath);
+
+            return;
+        }
+
+        std::string line;
+        std::string header;
+
+        while (std::getline(headerFile, line))
+        {
+            header.append(line + "\n");
+        }
+
+        ConsoleUI::HEADER = header;
+
+        headerFile.close();
+    }
+
+    std::string ConsoleUI::getColoredPiece(Colors color, Pieces piece) noexcept
+    {
+        std::string coloredPiece;
+
+        if (color == Colors::WHITE)
+        {
+            coloredPiece = std::format("{}{}{}", ConsoleUI::GREEN, ConsoleUI::pieceSymbol[piece], ConsoleUI::RESET);
+
+            return coloredPiece;
+        }
+        else if (color == Colors::BLACK)
+        {
+            coloredPiece = std::format("{}{}{}", ConsoleUI::RED, ConsoleUI::pieceSymbol[piece], ConsoleUI::RESET);
+
+            return coloredPiece;
+        }
+
+        LOG_WARN("Unknown color: {}", utils::toString(color));
+        return "";
+    }
+
+    std::string ConsoleUI::getSquare(State& state, int square) noexcept
+    {
+        for (int color = 0; color < Colors::COLORS; color++)
+        {
+            for (int piece = 0; piece < Pieces::PIECES; piece++)
+            {
+                Bitboard bb = state.allPieces[color][piece];
+                if (bb.isSet(square))
+                {
+                    return ConsoleUI::getColoredPiece(static_cast<Colors>(color), static_cast<Pieces>(piece));
+                }
+            }
+        }
+
+        // No piece, empty square
+        return " ";
+    }
+
+    void ConsoleUI::printHeader() noexcept
+    {
+        std::println("{}", ConsoleUI::HEADER);
+        std::println();
+    }
+
+    void ConsoleUI::printRow(State& state, int rank) noexcept
+    {
+        // Indent the printing
+        std::print(ConsoleUI::RANK_INDENT);
+
+        // Print rank specifier
+        std::print("{} ", rank + 1);
+
+        for (int file = 0; file < 8; file++)
+        {
+            int square = State::getSquareIndex(rank, file);
+            std::string squareVal = ConsoleUI::getSquare(state, square);
+
+            std::print("{}  {}  ", ConsoleUI::BORDER, squareVal);
+        }
+
+        // Print rank specifier
+        std::println("{} {}", ConsoleUI::BORDER, rank + 1);
+
+        // Print in-board line speparator
+        if (rank > 0)
+        {
+            std::println("{}{}", ConsoleUI::BOARD_INDENT, ConsoleUI::IN_LINE);
+        }
+    }
+
+    void ConsoleUI::printInfos(engine::board::State& state) noexcept
+    {
+        std::println("{}Half move clock : {}", ConsoleUI::RANK_INDENT, state.halfMoveClock);
+        std::println("{}Full move clock : {}", ConsoleUI::RANK_INDENT, state.fullMoveClock);
+        std::println("{}Side to move    : {}", ConsoleUI::RANK_INDENT, utils::toString(state.sideToMove));
+    }
+
+    void ConsoleUI::RenderState(engine::board::State& state) noexcept
+    {
+        ConsoleUI::printHeader();
+
+        // Print files specifiers
+        std::println("{}{}", ConsoleUI::FILE_IDENT, ConsoleUI::FILES);
+
+        // Print top line
+        std::println("{}{}", ConsoleUI::BOARD_INDENT, ConsoleUI::TOP_LINE);
+
+        // Print each row
+        for (int rank = 7; rank >= 0; rank--)
+        {
+            ConsoleUI::printRow(state, rank);
+        }
+
+        // Print the bottom line and files specifiers
+        std::println("{}{}", ConsoleUI::BOARD_INDENT, ConsoleUI::BOT_LINE);
+        std::println("{}{}", ConsoleUI::FILE_IDENT, ConsoleUI::FILES);
+
+        std::println();
+
+        ConsoleUI::printInfos(state);
+    }
+
+} // namespace ui::console
---
+++ src/ui/console/console_ui.h
@@ -0,0 +1,109 @@
+/**
+ * @file      console_ui.h
+ * @author    Thibault THOMAS
+ * @copyright Copyright 2025 Better Chess Engine
+ * @par       This project is released under the MIT License
+ *
+ * @brief Console rendering of the game.
+ */
+
+#include <array>
+#include <cstring>
+#include <string_view>
+
+#include "engine/board/state.h"
+#include "engine/core/enums.h"
+
+#ifndef CONSOLE_UI_H
+#define CONSOLE_UI_H
+
+/**
+ * @namespace ui_console
+ */
+namespace ui::console
+{
+    /**
+     * @class ConsoleUI
+     */
+    class ConsoleUI
+    {
+      public:
+        /**
+         * @brief Load the game's header from text file.
+         */
+        static void loadHeader() noexcept;
+
+        /**
+         * @brief Render the board and the current game state infos.
+         *
+         * @param [in] state : current state
+         */
+        static void RenderState(engine::board::State& state) noexcept;
+
+      private:
+        /**
+         * @brief Get a colored symbol for a given piece from a given team.
+         *
+         * @param [in] color : team's color
+         * @param [in] piece : piece to get
+         * @return std::string : the colored piece string
+         */
+        static std::string getColoredPiece(engine::core::Colors color, engine::core::Pieces piece) noexcept;
+
+        /**
+         * @brief Look through allPieces bitboard to get the piece on a specific square.
+         * If no piece found, return empty string for an empty square.
+         *
+         * @param [in] state  : current state
+         * @param [in] square : square to look on
+         * @return std::string : the colored piece symbol or empty string
+         */
+        static std::string getSquare(engine::board::State& state, int square) noexcept;
+
+        /**
+         * @brief Prints the header and blank lines after it.
+         */
+        static void printHeader() noexcept;
+
+        /**
+         * @brief Prints a full row with rank number specifier and square values.
+         *
+         * @param [in] state : current state
+         * @param [in] rank  : rank to print
+         */
+        static void printRow(engine::board::State& state, int square) noexcept;
+
+        /**
+         * @brief Prints the current state infos (side to move, clock, etc...)
+         *
+         * @param [in] state : current state
+         */
+        static void printInfos(engine::board::State& state) noexcept;
+
+        // ANSI escapes
+        static constexpr std::string_view RESET = "\033[0m";
+        static constexpr std::string_view RED = "\033[31m";
+        static constexpr std::string_view GREEN = "\033[32m";
+
+        static constexpr std::string_view FILES = "A     B     C     D     E     F     G     H";
+
+        static constexpr std::string_view TOP_LINE = "┌─────┬─────┬─────┬─────┬─────┬─────┬─────┬─────┐";
+
+        static constexpr std::string_view IN_LINE = "├─────┼─────┼─────┼─────┼─────┼─────┼─────┼─────┤";
+
+        static constexpr std::string_view BOT_LINE = "└─────┴─────┴─────┴─────┴─────┴─────┴─────┴─────┘";
+
+        static constexpr std::string_view BORDER = "│";
+
+        static constexpr std::array<std::string_view, engine::core::Pieces::PIECES> pieceSymbol = {
+            "", "", "󰡛", "󰡜", "󰡚", "󰡗"};
+
+        static constexpr std::string_view RANK_INDENT = "            ";
+        static constexpr std::string_view BOARD_INDENT = "              ";
+        static constexpr std::string_view FILE_IDENT = "                 ";
+
+        static inline std::string HEADER = "";
+    };
+} // namespace ui::console
+
+#endif // CONSOLE_UI_H
