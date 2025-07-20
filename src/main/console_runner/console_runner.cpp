9a816fe4af2af081626c52af743f85c6f535e946
newlysama <thibault.thomas71@gmail.com>
2025-07-20 10:21:12 +0200

    add: console user interface

---
+++ src/main/console_runner/console_runner.cpp
@@ -0,0 +1,236 @@
+/**
+ * @file      console_runner.cpp
+ * @author    Thibault THOMAS
+ * @copyright Copyright 2025 Better Chess Engine
+ * @par       This project is released under the MIT License
+ *
+ * @brief Implementation of the game loop in console mode.
+ */
+
+#include "main/console_runner/console_runner.h"
+
+#include <iostream>
+#include <print>
+
+#include "engine/core/const.h"
+#include "logging/logging.h"
+#include "ui/console/console_ui.h"
+
+#if !defined(BUILD_RELEASE) && !defined(BUILD_BENCHMARK)
+#include "utils/enums_to_string.h"
+#include "utils/utils.h"
+#endif
+
+/**
+ * @namespace console_runner
+ */
+namespace console_runner
+{
+    using namespace ui::console;
+    using namespace engine::core;
+    using namespace engine::game;
+
+    ConsoleRunner::ConsoleRunner() noexcept
+        : game(Game{})
+    {
+    }
+
+    std::string ConsoleRunner::askInput() const noexcept
+    {
+        std::string input;
+
+        if (this->game.state.halfMoveClock >= 100)
+        {
+            std::print("Select a move to play (format ex: a1a2 OR 'draw'): ");
+        }
+        else
+        {
+            std::print("Select a move to play (format ex: a1a2): ");
+        }
+
+        std::cin >> input;
+
+        LOG_DEBUG("Got user console input: {}", input);
+
+        return input;
+    }
+
+    std::string ConsoleRunner::askDraw() const noexcept
+    {
+        std::string input;
+        std::print("{} player asked for a draw, accept ? (yes/no): ", utils::toString(this->game.state.sideToMove));
+
+        while (true)
+        {
+            std::cin >> input;
+
+            if (input == "yes" || input == "no")
+            {
+                LOG_DEBUG("Draw response from enemy player: {}", input);
+                return input;
+            }
+            else
+            {
+                std::print("Invalid response: [{}], please enter yes/no : ", input);
+            }
+        }
+    }
+
+    Pieces ConsoleRunner::askPromotion() const noexcept
+    {
+        std::string promotion;
+        Pieces piece;
+
+        std::print("Select a promotion type (lowercase piece name): ");
+
+        while (true)
+        {
+            std::cin >> promotion;
+            piece = utils::fromString(promotion);
+
+            if (piece == Pieces::UNKNOWN_PIECE || piece == Pieces::KING || piece == Pieces::PAWN) [[unlikely]]
+            {
+                LOG_DEBUG("User entered non existing piece: {}", promotion);
+                std::print("Please enter a valid piece (queen, rook, bishop or knight): ");
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        return piece;
+    }
+
+    std::expected<Move, std::string> ConsoleRunner::inputToMove(std::string input) noexcept
+    {
+        if (input.size() != 4)
+        {
+            return std::unexpected(std::format("Invalid input size: {}", input.size()));
+        }
+
+        std::string fromStr = input.substr(0, 2);
+        std::string toStr = input.substr(2, 2);
+
+        int fromSquare;
+        int toSquare;
+
+        // Check if from square input is valid
+        if (SQUARE_INDEX.find(fromStr) != SQUARE_INDEX.end())
+        {
+            fromSquare = SQUARE_INDEX.at(fromStr);
+        }
+        else
+        {
+            return std::unexpected(std::format("Entered an initial square does not exist: {}", fromStr));
+        }
+
+        // Check if destination square input is valid
+        if (SQUARE_INDEX.find(toStr) != SQUARE_INDEX.end())
+        {
+            toSquare = SQUARE_INDEX.at(toStr);
+        }
+        else
+        {
+            return std::unexpected(std::format("Entered a destination square does not exist: {}", toStr));
+        }
+
+        // If the requested move exists, return it.
+        for (std::size_t i = 0; i < this->game.moveList.size(); i++)
+        {
+            Move current = this->game.moveList[i];
+
+            if (current.fromSquare == fromSquare && current.toSquare == toSquare)
+            {
+                return current;
+            }
+        }
+
+        return std::unexpected(std::format("Entered move is not legal: {} --> {}", fromStr, toStr));
+    }
+
+    bool ConsoleRunner::handleDrawRequest() noexcept
+    {
+        LOG_DEBUG("Player {} is calling a draw", utils::toString(this->game.state.sideToMove));
+
+        if (this->game.state.halfMoveClock >= 100)
+        {
+            while (true)
+            {
+                // Ask the enemy player his response to the draw request
+                std::string drawResponse = this->askDraw();
+
+                if (drawResponse == "yes")
+                {
+                    LOG_DEBUG("Both players acctepted the draw. Ending the this->");
+                    std::println("Game ended on a draw");
+
+                    return true;
+                }
+                else
+                {
+                    LOG_DEBUG("Enemy player refused the draw request, continuing");
+                    std::println("Enemy player refused the draw request, continuing");
+
+                    return false;
+                }
+            }
+        }
+        else
+        {
+            LOG_DEBUG("Draw request is not valid, current Half Move Clock is {}", this->game.state.halfMoveClock);
+            std::println("Cannot draw until Half Move Clock reaches 100.");
+
+            return false;
+        }
+    }
+
+    void ConsoleRunner::runGame() noexcept
+    {
+        this->game.moveList.generateAllMoves(this->game.state);
+
+        // Load UI header
+        ConsoleUI::loadHeader();
+
+        // Print initial state
+        ConsoleUI::RenderState(this->game.state);
+
+        while (true)
+        {
+            std::string userInput = this->askInput();
+
+            if (userInput == "draw")
+            {
+                bool isDraw = this->handleDrawRequest();
+
+                if (isDraw)
+                {
+                    return;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            auto move = this->inputToMove(userInput);
+
+            if (!move)
+            {
+                LOG_DEBUG(move.error());
+                std::println("{}", move.error());
+                continue;
+            }
+
+            if (move.value().promotion == true) [[unlikely]]
+            {
+                move.value().promotionPiece = this->askPromotion();
+            }
+
+            this->game.playMove(move.value());
+
+            // Print new state
+            ConsoleUI::RenderState(this->game.state);
+        }
+    }
+} // namespace console_runner
---
+++ src/main/console_runner/console_runner.h
@@ -0,0 +1,88 @@
+/**
+ * @file      console_runner.h
+ * @author    Thibault THOMAS
+ * @copyright Copyright 2025 Better Chess Engine
+ * @par       This project is released under the MIT License
+ *
+ * @brief Handles the game loop in console mode.
+ */
+
+#ifndef CONSOLE_RUNNER_H
+#define CONSOLE_RUNNER_H
+
+#include <expected>
+
+#include "engine/core/enums.h"
+#include "engine/game/game.h"
+
+/**
+ * @namespace console_runner
+ */
+namespace console_runner
+{
+    /**
+     * @class ConsoleRunner
+     */
+    class ConsoleRunner
+    {
+      public:
+        /**
+         * @brief Constructor : instantiate the game
+         */
+        ConsoleRunner() noexcept;
+
+        /**
+         * @brief Default destructor.
+         */
+        ~ConsoleRunner() noexcept = default;
+
+        /**
+         * @brief Main loop function to run the game.
+         */
+        void runGame() noexcept;
+
+      private:
+        /**
+         * @brief Ask the user the move he wants to play.
+         *
+         * @return std::string : the user's input
+         */
+        std::string askInput() const noexcept;
+
+        /**
+         * @brief If previous player called a draw, ask it to the other player.
+         *
+         * @return std::string : response from other player (yes/no)
+         */
+        std::string askDraw() const noexcept;
+
+        /**
+         * @brief Asks user for promotion.
+         * Restart until user enters a valid promotion.
+         *
+         * @return Pieces : the requested piece type.
+         */
+        engine::core::Pieces askPromotion() const noexcept;
+
+        /**
+         * @brief Parses a user input and use it to build a move.
+         *
+         * @param [in] input : the user input
+         * @return std::expected : built move if input is valid or error message if not valid
+         */
+        std::expected<engine::game::Move, std::string> inputToMove(std::string input) noexcept;
+
+        /**
+         * @brief Handles a draw request from a user.
+         *
+         * @return bool : wether the draw is valid and accepted by both users
+         */
+        bool handleDrawRequest() noexcept;
+
+        // Game engine instance
+        engine::game::Game game;
+    };
+
+} // namespace console_runner
+
+#endif // CONSOLE_RUNNER_H
