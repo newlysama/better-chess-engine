128c33ee4ba558d0b672b1d13fd136c839a07223
newlysama <thibault.thomas71@gmail.com>
2025-08-11 11:59:28 +0200

    add: default constructors / destructors + documentation

--- src/server/snapshot/snapshots.h
+++ src/server/snapshot/snapshots.h
@@ -4,7 +4,7 @@
  * @copyright Copyright 2025 Better Chess Engine
  * @par       This project is released under the MIT License
  *
- * @brief Declare snapshot structures (payloads).
+ * @brief Declare snapshot structures (meant to built / decode payloads).
  */
 
 #ifndef SNAPSHOTS_H_
@@ -14,32 +14,45 @@
 #include <optional>
 #include <vector>
 
-#include "engine/game/move.h"
 #include "server/core/types.h"
 
 namespace server::snapshot
 {
-    struct MoveSnapshot
+    /**
+     * @typedef MoveSnapshot
+     * @brief   C++ interface with RapidJson for Move representation.
+     */
+    typedef struct MoveSnapshot
     {
         int fromSquare;
         int toSquare;
         std::optional<int> capturedSquare;
         std::optional<std::string> promotionPiece;
         std::optional<std::string> castlingType;
-    };
 
-    struct GameSnapshot
+        MoveSnapshot() = default;
+        ~MoveSnapshot() = default;
+    } MoveSnapshot;
+
+    /**
+     * @typedef GameSnapshot
+     * @brief   C++ interface with RapidJson for Game state representation.
+     */
+    typedef struct GameSnapshot
     {
         core::RoomId roomId;
-        std::string fen;
+        std::string fen; // Only the occupancy part (ex: 8/8/8/8/8/8/8/8)
         std::string turn;
         uint16_t halfmove;
         uint16_t fullmove;
         bool inCheck;
         bool checkMate;
-        MoveSnapshot lastMove;
+        MoveSnapshot lastMove; // The move that was just played
         std::vector<MoveSnapshot> legalMoves;
-    };
+
+        GameSnapshot() = default;
+        ~GameSnapshot() = default;
+    } GameSnapshot;
 } // namespace server::snapshot
 
 #endif // SNAPSHOTS_H_
